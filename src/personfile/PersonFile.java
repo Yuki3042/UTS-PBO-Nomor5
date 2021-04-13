06332e26e601d14ac328c9738b01f8e7bdae332d
Your Name <you@example.com>
2021-04-13 14:26:04 +0700

    Tugas UTS mata kuliah PBO Nomor 5 oleh Dito Arya Saputra NIM 20050974038 dari kelas PTI B 2020

---
+++ src/personfile/PersonFile.java
@@ -0,0 +1,11 @@
+package personfile;
+
+public class PersonFile {
+  
+    public static void main(String[] args) {
+       PreferredCustomer preferredCustomer = new PreferredCustomer
+        ("Agung Hartono","East Street",15983,1202,true,2100);
+       System.out.println(preferredCustomer.toString());
+    }
+    
+}
